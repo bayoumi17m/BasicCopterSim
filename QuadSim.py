ba43a410c3ec79e731671fcc32bca4cdbbe52074
Magd <mb2363@cornell.edu>
2018-06-18 18:38:46 -0400

    structural files

--- QuadSim.py
+++ QuadSim.py
@@ -1,6 +1,7 @@
-import quadcopter,gui,controller
+import copter,gui,controller
 import signal
 import sys
+
 
 
 # Constants
@@ -9,24 +10,49 @@ QUAD_DYNAMICS_UPDATE = 0.002 # seconds
 CONTROLLER_DYNAMICS_UPDATE = 0.005 # seconds
 run = True
 
+def SingleTest():
+    # Set goals to go to
+    GOALS = [(1,1,2),(1,-1,4),(-1,-1,2),(-1,1,4)]
+    YAWS = [0,3.14,-1.54,1.54]
+    # Define the quadcopters
+    QUADCOPTER={'q1':{'position':[1,0,4],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
+    # Controller parameters
+    CONTROLLER_PARAMETERS = {'Motor_limits':[4000,9000],
+                        'Tilt_limits':[-10,10],
+                        'Yaw_Control_Limits':[-900,900],
+                        'Z_XY_offset':500,
+                        'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
+                        'Linear_To_Angular_Scaler':[1,1,0],
+                        'Yaw_Rate_Scaler':0.18,
+                        'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
+                        }
 
-# Set goals to go to
-GOALS = [(1,1,2),(1,-1,4),(-1,-1,2),(-1,1,4)]
-YAWS = [0,3.14,-1.54,1.54]
-# Define the quadcopters
-QUADCOPTER={'q1':{'position':[1,0,4],'orientation':[0,0,0],'L':0.3,'r':0.1,'prop_size':[10,4.5],'weight':1.2}}
-# Controller parameters
-CONTROLLER_PARAMETERS = {'Motor_limits':[4000,9000],
-                    'Tilt_limits':[-10,10],
-                    'Yaw_Control_Limits':[-900,900],
-                    'Z_XY_offset':500,
-                    'Linear_PID':{'P':[300,300,7000],'I':[0.04,0.04,4.5],'D':[450,450,5000]},
-                    'Linear_To_Angular_Scaler':[1,1,0],
-                    'Yaw_Rate_Scaler':0.18,
-                    'Angular_PID':{'P':[22000,22000,1500],'I':[0,0,1.2],'D':[12000,12000,0]},
-                    }
+    # Catch Ctrl+C to stop threads
+    signal.signal(signal.SIGINT, signal_handler)
+    # Make objects for quadcopter, gui and controller
+    quad = copter.Quadcopter(QUADCOPTER)
+    gui_object = gui.GUI(quads=QUADCOPTER)
+        ctrl = controller.Controller_Point2Point(quad.get_state,quad.get_time,quad.set_motor_speeds,params=CONTROLLER_PARAMETERS,quad_identifier='q1')
+        # Start the threads
+        quad.start_thread(dt=QUAD_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
+        ctrl.start_thread(update_rate=CONTROLLER_DYNAMICS_UPDATE,time_scaling=TIME_SCALING)
+        # Update the GUI while switching between destination poitions
+        while(run==True):
+            for goal,y in zip(GOALS,YAWS):
+                ctrl.update_target(goal)
+                ctrl.update_yaw_target(y)
+                for i in range(300):
+                    gui_object.quads['q1']['position'] = quad.get_position('q1')
+                    gui_object.quads['q1']['orientation'] = quad.get_orientation('q1')
+                    gui_object.update()
+        quad.stop_thread()
+        ctrl.stop_thread()
 
-# Catch Ctrl+C to stop threads
-signal.signal(signal.SIGINT, signal_handler)
-# Make objects for quadcopter, gui and controller
-quad = quadcopter.Quadcopter(QUADCOPTER)
+def signal_handler(signal, frame):
+    global run
+    run = False
+    print('Stopping')
+    sys.exit(0)
+
+if __name__ == "__main__":
+    SingleTest()

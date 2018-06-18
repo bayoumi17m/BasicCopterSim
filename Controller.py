e2e194be7d97026db5fdd1af835037c3f2b802c4
Magd <mb2363@cornell.edu>
2018-06-18 18:42:34 -0400

    Added Point2Point Controller Class

---
+++ Controller.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+import time
+import threading
+
+
+class Controller_Point2Point(object):
+    """docstring forController_Point2Point."""
+    def __init__(self, arg):
+        self.arg = arg
